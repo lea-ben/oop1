e589f6045033f230d342be82c6c723c440518abe
lea-ben <leabenillouche@MacBook-Pro-de-Lea.local>
2021-02-07 22:25:23 +0200

    first

---
+++ oop.py
@@ -0,0 +1,105 @@
+# class PlayerCharacter : 
+#     membership = True
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+#     def shout(self):
+#         print(f"my name is {self.name}")
+#         return "work"
+
+    
+
+# player1 = PlayerCharacter("lea",22)
+# player2 = PlayerCharacter("leontine",44)
+
+# print(player1.name)
+# print(player1.age)
+# print(player1.run())
+
+# class PlayerCharacter : 
+
+#     membership = True 
+#     def __init__(self, name,age):
+#          self.name = name
+#          self.age = age
+
+#     def shout(self):
+#         print (f"my name is {self.name}")
+
+    
+
+# player1 = PlayerCharacter("lea",20)
+
+# # print(player1.shout())
+#                                         EXERCISE 1
+
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#          #         print(f"{cat1} is older")
+
+
+#     def main():
+#         cat1 = Cat("firstcat",1)
+#         cat2 = Cat("secondcat",2)
+#         cat3 = Cat("thirdcat",3)
+
+#         # print(cat0)
+#         findOld(cat1,cat2,cat3)
+
+    # def oldest_cat(self): 
+    #     return max(self.age)
+    #     print(f"the oldest cat is {self.name} and is {age} years old")
+
+
+
+                    # EXERCISE 2
+
+
+# class Dog : 
+#     def __init__(self, nameDog, heightDog)
+#     self.nameDog = nameDog 
+#     self.heightDog = heightDog
+
+
+# def talk(self): 
+#     print("wouf")
+
+# def jump(self):
+#     height2 = self.heightDog *2
+#     print(f"when {self.nameDog} is jumping is height is {height2}")
+
+
+# # Outside of the class, create an object davids_dog. His dog’s name is “Rex” and his height is 50cm.
+# # Print the details of his dog by calling the methods.
+# # Create an object sarahs_dog. Her dog’s name is “Teacup” and his height is 20cm.
+# # Print the details of her dog by calling the methods.
+# # Create an if statement outside of the class to check which dog is bigger. Print the name of the bigger dog.
+
+
+                                                # EXERCISE3
+
+
+# class Song : 
+#     def __init__(self, lyrics) 
+
+
+# self.lyrics = lyrics 
+#     print ("There’s a lady who's sur all that glitters is gold and she’s buying a stairway to heaven")
+ 
+
+class ZOO :
+
+    def __init__(self,zoo_name,animal,name)
+        self.zoo_name = zoo_name
+        self.animal = animal 
+        self.name = name
+
+
+Create a class Zoo
+In this class create a method __init__ that takes one parameter: zoo_name
+It instantiates two attributes: animals (an empty list) and name (name of the zoo).
+Create a method add_animal that takes one parameter new_animal. This method adds the new_animal to the animals list, only if the new_animal isn’t already in the list.
